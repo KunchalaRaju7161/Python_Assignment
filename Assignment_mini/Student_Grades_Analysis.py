25c962298751beb3d990b0b00a5c40aa61392b60
KunchalaRaju7161 <102028984+KunchalaRaju7161@users.noreply.github.com>
2023-07-17 16:05:07 +0530

    I have complete Python Problem-1

--- Assignment_mini/Student_Grades_Analysis.py
+++ Assignment_mini/Student_Grades_Analysis.py
@@ -8,6 +8,7 @@ path = "/Users/rkunchala/pythonAssignments/Python_Assignments/Assignment_mini/Re
 #  object is created
 wb_obj = openpyxl.load_workbook(path)
 
+
 # Get workbook active sheet object
 # from the active attribute
 sheet_obj = wb_obj.active

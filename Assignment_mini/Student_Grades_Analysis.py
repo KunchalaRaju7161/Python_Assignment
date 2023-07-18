ecd1dee831141a562c863aba9521e361c450522c
KunchalaRaju7161 <102028984+KunchalaRaju7161@users.noreply.github.com>
2023-07-18 16:08:48 +0530

    I have completed Student Grade Analysis python problem and I have written the comments

--- Assignment_mini/Student_Grades_Analysis.py
+++ Assignment_mini/Student_Grades_Analysis.py
@@ -14,6 +14,9 @@ wb_obj = openpyxl.load_workbook(path)
 sheet_obj = wb_obj.active
 max_col = sheet_obj.max_column
 
+# create empty Alice array
+# Loop will get the all grades of Alice
+# Find Alice highest average grade
 Array_Alice = []
 for i in range(2, max_col + 1):
     cell_obj = sheet_obj.cell(row=1, column=i)
@@ -23,16 +26,22 @@ Alice_Average = (Array_Alice[0] + Array_Alice[1] + Array_Alice[2]) / 3
 print("Student: ", sheet_obj.cell(row=1, column=1).value)
 print("Average Grade: ", Alice_Average)
 
+# create empty Bob array
+# Loop will get all grades of Bob
+# Find Bob highest average grade
 Array_Bob = []
 for i in range(2, max_col + 1):
     cell_obj1 = sheet_obj.cell(row=2, column=i)
     Array_Bob.append(cell_obj1.value)
+
 Bob_Average = (Array_Bob[0] + Array_Bob[1] + Array_Bob[2]) / 3
 print("Student: ", sheet_obj.cell(row=2, column=1).value)
 print("Average Grade: ", Bob_Average)
 
+#  create empty Alice array
+# Loop will get all grades of Charlie
+# Find Charlie highest average grade
 Array_Charlie = []
-# Loop will print all grades of Charlie
 for i in range(2, max_col + 1):
     cell_obj2 = sheet_obj.cell(row=3, column=i)
     Array_Charlie.append(cell_obj2.value)
@@ -41,8 +50,10 @@ Charlie_Average = (Array_Charlie[0] + Array_Charlie[1] + Array_Charlie[2]) / 3
 print("Student : ", sheet_obj.cell(row=3, column=1).value)
 print("Average Grade : ", Charlie_Average)
 
+#  create empty David array
+# Loop will get all grades of David
+# Find David highest average grade
 Array_David = []
-# Loop will print all grades of David
 for i in range(2, max_col + 1):
     cell_obj3 = sheet_obj.cell(row=4, column=i)
     Array_David.append(cell_obj3.value)
@@ -51,6 +62,8 @@ David_Average = (Array_David[0] + Array_David[1] + Array_David[2]) / 3
 print("Student : ", sheet_obj.cell(row=4, column=1).value)
 print("Average Grade : ", David_Average)
 
+# Find the student with the highest average grade
+# print the Result
 print("Student with the highest average grade: ")
 highest_average_grade = max(Alice_Average, Bob_Average, Charlie_Average, David_Average)
 

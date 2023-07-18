ae67e622d2b2e77c84833a9bb2dd7aaee61b539a
KunchalaRaju7161 <102028984+KunchalaRaju7161@users.noreply.github.com>
2023-07-18 16:51:03 +0530

    I have completed bonacci Sequence Generator python problem and I have written the comments

--- Assignment_mini/Fibonacci_Sequence_problem/Fibonacci_Sequence.py
+++ Assignment_mini/Fibonacci_Sequence_problem/Fibonacci_Sequence.py
@@ -1,3 +1,6 @@
+# Define the decorator function
+# generator_func() -It invokes the original generator function passed as an argument
+# fib_gen - to create an iterator object
 def fibonacci_decorator(generator_func):
     def fibonacci_generator():
         fib_gen = generator_func()
@@ -9,6 +12,7 @@ def fibonacci_decorator(generator_func):
     return fibonacci_generator
 
 
+# Apply the decorator to the Fibonacci sequence generator function
 @fibonacci_decorator
 def fibonacci_sequence():
     a, b = 0, 1
@@ -17,6 +21,10 @@ def fibonacci_sequence():
         a, b = b, a + b
 
 
+# Test the generator by calling it in a loop and printing the generated even Fibonacci numbers.
 fib_gen = fibonacci_sequence()
+
+# It sets up a loop that will iterate 10 times.
+# The loop variable _ is used when we don't need to use the loop counter value.
 for _ in range(10):
     print(next(fib_gen))
